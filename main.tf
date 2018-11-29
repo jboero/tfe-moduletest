32ef4cd90b9b02c0a54f03432fed03ea0b79a6c8
JohnnyB <jboero@users.noreply.github.com>
2018-11-29 15:52:10 +0000

    Testing
    This is a test.

---
+++ main.tf
@@ -0,0 +1,18 @@
+
+//--------------------------------------------------------------------
+// Modules
+module "ec2_instance" {
+  source  = "app.terraform.io/JoeStack/ec2-instance/aws"
+  version = "1.13.0"
+
+  ami = "ami-048fac6b039d41144"
+  instance_type = "t2.small"
+  name = "jboero-testing"
+  subnet_id = "${module.vpc.private_subnets}"
+  vpc_security_group_ids = "${module.vpc.default_security_group_id}"
+}
+
+module "vpc" {
+  source  = "app.terraform.io/JoeStack/vpc/aws"
+  version = "1.46.0"
+}
